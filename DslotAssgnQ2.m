8bb287e35a50748d9924d28f86735c6655641004
dk175814 <36330129+dk175814@users.noreply.github.com>
2018-11-24 11:31:10 +0530

    Update DslotAssgnQ2.m

--- DslotAssgnQ2.m
+++ DslotAssgnQ2.m
@@ -1,8 +1,9 @@
+%QUESTION 2:
 clc;
 clear;
 damp = [0.01 0.05 0.2 0.5]; % damping ratio
 for i = 1:4
     figure();
-    fplot(@(x) 1/sqrt((1-x^2)^2+(2*damp(i)*x)^2),[0 5]);
+    fplot(@(y) 1/sqrt((1-y^2)^2+(2*damp(i)*y)^2),[0 5]); %Formula for DAF
 end
 
