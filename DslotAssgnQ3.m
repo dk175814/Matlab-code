33bcd94b1ce5e044b0fe3a7f152d6f4d6678296c
dk175814 <36330129+dk175814@users.noreply.github.com>
2018-11-24 11:32:57 +0530

    Update DslotAssgnQ3.m

--- DslotAssgnQ3.m
+++ DslotAssgnQ3.m
@@ -1,3 +1,4 @@
+%QUESTION 3:
 k = 3600;
 m = 100;
 zeta = 0.02; %damping ratio
@@ -6,10 +7,14 @@ load elcentro.mat %load exciting force data (file should be in the current folde
 
 %duhamel's integral 
 wd = wn*sqrt(1-zeta*zeta); %frequency of vibration with damping
+
 h = (exp(-1*zeta*wn*t).*sin(wd*t))./(m*wd); %response for unit impulse
+
 dt = t(2)-t(1); %sampling time
 X(1) = 0; %initial condition
+
 for i=2:1:length(t)
+
 	X(i)=sum(p(1:i-1).*flip(h(1:i-1)))*dt; %integration (p multiplied with fliped h and summed up)
 end
 plot(t,X); %plot
